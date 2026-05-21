a4e714f6eed626d1e37046216f627afc3a44b6fd
Martin Halleland <martin.halleland@gmail.com>
2026-05-21 10:39:42 +0200

    TH-2722: Lager Application

--- apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Main.kt
+++ apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Main.kt
@@ -1,11 +1,5 @@
 package no.kartverket.no.kartverket.matrikkel.broker
 
-import io.ktor.server.netty.*
-
 fun main() {
-    KtorServer.create(factory = Netty, port = 8081){
-        //konfigurer her
-        configureRouting()
-
-    }.start(wait = true)
+    runApplication()
 }
