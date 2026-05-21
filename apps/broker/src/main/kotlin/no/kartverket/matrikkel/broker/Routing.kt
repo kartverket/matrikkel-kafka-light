336b96faeac2a7bb0bce602234aa886d0a47102a
Martin Halleland <martin.halleland@gmail.com>
2026-05-21 13:00:06 +0200

    TH-2722: Enkle health sjekker

--- apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Routing.kt
+++ apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Routing.kt
@@ -1,5 +1,6 @@
 package no.kartverket.matrikkel.broker
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +9,20 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello, World!")
+        }
+
+        route("internal") {
+            get("isAlive") {
+                    call.respondText("Alive")
+            }
+
+            get("isReady") {
+                    call.respondText("Ready")
+            }
+
+            get("selftest") {
+                call.respondText("Helloworld")
+            }
         }
     }
 }
