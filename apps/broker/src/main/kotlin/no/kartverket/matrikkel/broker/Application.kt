673956329652fe440cd41835445305eaa79589a7
Martin Halleland <martin.halleland@gmail.com>
2026-05-27 14:59:44 +0200

    nojira - test av dispatch

--- apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Application.kt
+++ apps/broker/src/main/kotlin/no/kartverket/matrikkel/broker/Application.kt
@@ -5,8 +5,8 @@ import no.kartverket.matrikkel.broker.config.Configuration
 import no.kartverket.matrikkel.broker.config.DataSourceConfiguration
 
 fun runApplication() {
-    val config = Configuration()
-    DataSourceConfiguration.migrate(config.database)
+//    val config = Configuration()
+//    DataSourceConfiguration.migrate(config.database)
 
     KtorServer.create(factory = Netty, port = 8081){
         //konfigurer her
