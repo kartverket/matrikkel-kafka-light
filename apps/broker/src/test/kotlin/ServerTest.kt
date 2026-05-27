327bfde6563e2c67b2ccabce0ec5252da2694da3
Nicklas Utgaard <nutgaard@gmail.com>
2026-05-27 15:28:33 +0200

    nojira: fikse banal test

--- apps/broker/src/test/kotlin/ServerTest.kt
+++ apps/broker/src/test/kotlin/ServerTest.kt
@@ -3,15 +3,16 @@ package no.kartverket
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.testApplication
+import no.kartverket.matrikkel.broker.configureRouting
 import kotlin.test.*
 
 class ServerTest {
 
     @Test
     fun `test root endpoint`() = testApplication {
-        // loads default configuration
-        configure()
-        // verify server root returns 200
+        application {
+            configureRouting()
+        }
         assertEquals(HttpStatusCode.OK, client.get("/").status)
     }
 
