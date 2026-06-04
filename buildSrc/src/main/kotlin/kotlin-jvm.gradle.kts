3f566fb4fc2173ed21188d4f1095a9230efa6f05
Nicklas Utgaard <nutgaard@gmail.com>
2026-06-04 14:28:10 +0200

    noJira: fikser konfigurasjon av kotlinx serialization

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -7,6 +7,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin in JVM projects.
     kotlin("jvm")
+    kotlin("plugin.serialization")
 }
 
 kotlin {
