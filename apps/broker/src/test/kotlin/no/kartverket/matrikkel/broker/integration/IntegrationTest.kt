8fb305caea85619531b976454d61e9903a9930fe
Nicklas Utgaard <nutgaard@gmail.com>
2026-08-21 12:54:54 +0200

    TH-3093: integrasjonstest med flere produsenter og konsumenter

--- apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/integration/IntegrationTest.kt
+++ apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/integration/IntegrationTest.kt
@@ -1,18 +1,20 @@
 package no.kartverket.matrikkel.broker.integration
 
 import assertk.assertThat
+import assertk.assertions.each
 import assertk.assertions.hasSize
 import assertk.assertions.isEqualTo
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.request.header
+import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.cbor.*
 import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.testing.*
+import kotlinx.coroutines.*
 import kotlinx.serialization.cbor.Cbor
 import no.kartverket.heimdall.common.ktor.plugins.security.Security
 import no.kartverket.matrikkel.broker.api.topicRoutes
@@ -26,6 +28,7 @@ import no.kartverket.no.kartverket.matrikkel.broker.testutils.WithDatabase
 import org.junit.jupiter.api.Test
 import java.util.*
 import kotlin.io.encoding.Base64
+import kotlin.time.Duration.Companion.seconds
 
 private typealias ProducerFactory = (username: String, topic: String) -> MessageProducer<String, String>
 private typealias ConsumerFactory = (username: String, topic: String) -> MessageConsumer<String, String>
@@ -40,10 +43,11 @@ class IntegrationTest : WithDatabase {
                 )
             ),
             Topic(
-                name = "seciond-topic", acl = TopicAccessControlList(
-                    publishIdentities = setOf(),
-                    consumeIdentities = setOf(),
-                )
+                name = "second-topic", acl = TopicAccessControlList(
+                    publishIdentities = setOf("p1", "p2"),
+                    consumeIdentities = setOf("c1", "c2"),
+                ),
+                leaseTime = 2.seconds
             ),
         )
     )
@@ -67,8 +71,93 @@ class IntegrationTest : WithDatabase {
         }
     }
 
+    @Test
+    fun `multiple producers and consumers scenario`() {
+        val messagesPerClient = 500
+        val producerIds = listOf("p1" to "second-topic", "p2" to "second-topic")
+        val consumerIds = listOf("c1" to "second-topic", "c2" to "second-topic")
+
+        runIntegrationTest { producerFactory, consumerFactory ->
+            val startupSignal = CompletableDeferred<Unit>()
+
+            val producers = producerIds.map { (id, topic) ->
+                launch(Dispatchers.IO) {
+                    val producer = producerFactory(id, topic)
+                    startupSignal.await()
+
+                    producer.produceMessages {
+                        repeat(messagesPerClient) {
+                            yield(ProducerRecord("$id-$it", "$id-$it"))
+                        }
+                    }
+
+                    producer.close()
+                }
+            }
+            val consumers = consumerIds.map { (id, topic) ->
+                async(Dispatchers.IO) {
+                    val consumer = consumerFactory(id, topic)
+                    startupSignal.await()
+                    consumer.use { it.consumeMessages(messagesPerClient) }
+                }
+            }
+
+            startupSignal.complete(Unit)
+
+            joinAll(*producers.toTypedArray())
+            val records = awaitAll(*consumers.toTypedArray())
+            val allRecords = records.flatten()
+
+            // Records delivered exactly once
+            val expectedNumberOfRecords = messagesPerClient * producerIds.size
+            assertThat(allRecords).hasSize(expectedNumberOfRecords)
+            assertThat(allRecords.distinctBy { it.key }).hasSize(expectedNumberOfRecords)
+            assertThat(allRecords.distinctBy { it.sequence }).hasSize(expectedNumberOfRecords)
+
+            // The sequence has no gaps or duplicates
+            val fullsequence = allRecords.map { it.sequence }.sorted()
+            assertThat(fullsequence).isEqualTo((1..expectedNumberOfRecords.toLong()).toList())
+
+            // Both consumers have received records
+            assertThat(records).each {
+                it.hasSize(messagesPerClient)
+            }
+
+            // No records was delivered to both consumers
+            val (firstRecords, secondRecords) = records
+            val firstRecordSeqs = firstRecords.map { it.sequence }.toSet()
+            val secondRecordSeqs = secondRecords.map { it.sequence }.toSet()
+            assertThat(firstRecordSeqs.intersect(secondRecordSeqs)).isEqualTo(emptySet())
+
+            // Every consumer has seen a monotonically increasing sequence list
+            for (consumerRecords in records) {
+                val sequence = consumerRecords.map { it.sequence }
+                assertThat(sequence).isEqualTo(sequence.sorted())
+            }
+        }
+    }
+
+    private suspend fun MessageProducer<String, String>.produceMessages(
+        block: suspend SequenceScope<ProducerRecord<String, String>>.() -> Unit
+    ): MessageProducer<String, String> {
+        for (record in sequence(block)) {
+            this.send(record)
+        }
+        return this
+    }
+
+    private suspend fun MessageConsumer<String, String>.consumeMessages(n: Int): List<ConsumerRecord<String, String>> {
+        val records = mutableListOf<ConsumerRecord<String, String>>()
+        do {
+            val response = this.poll(maxRecords = n - records.size)
+            records.addAll(response.records)
+            this.commitSync()
+        } while (records.size < n)
+        return records
+    }
+
     private fun WithDatabase.runIntegrationTest(
-        block: suspend ApplicationTestBuilder.(ProducerFactory, ConsumerFactory) -> Unit
+        block: suspend CoroutineScope.(ProducerFactory, ConsumerFactory) -> Unit
     ) {
         val security = Security(
             Security.AuthProvider(
@@ -100,6 +189,8 @@ class IntegrationTest : WithDatabase {
 
             val clientFactory = { username: String ->
                 createClient {
+                    expectSuccess = true
+
                     install(ContentNegotiation) {
                         cbor(
                             Cbor {
@@ -137,15 +228,18 @@ class IntegrationTest : WithDatabase {
                         keySerializer = StringSerde,
                         valueSerializer = StringSerde,
                         consumerGroup = "group-1",
-                        instanceId = "instance-1",
+                        instanceId = username,
                         initialOffsetPolicy = InitialOffsetPolicy.EARLIEST,
-                        correlationIdProvider = { UUID.randomUUID().toString() }
+                        correlationIdProvider = { UUID.randomUUID().toString() },
+                        timeout = 1.seconds
                     ),
                     client = clientFactory(username)
                 )
             }
 
-            block(producerFactory, consumerFactory)
+            coroutineScope {
+                block(producerFactory, consumerFactory)
+            }
         }
     }
 
