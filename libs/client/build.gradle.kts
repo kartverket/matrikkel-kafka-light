96cdb1728d96ac6c910a17767b406eb65fa3829c
Mathias Gaptjern <Mathias.Gaptjern@kartverket.no>
2026-09-14 12:20:51 +0200

    TH-3187 Legger til oppsett for å publisere klienter til maven

--- libs/client/build.gradle.kts
+++ libs/client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("buildsrc.convention.kotlin-jvm")
+    id("maven-publish")
 }
 
 dependencies {
@@ -13,4 +14,28 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(libs.bundles.testEcosystem)
     testImplementation(libs.bundles.testEcosystem)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "no.kartverket.matrikkel"
+            artifactId = "matrikkel-kafka-light"
+            version = providers.systemProperty("suffix").orElse("0-SNAPSHOT").get()
+            pom {
+                description = "Implementation of no.kartverket.matrikkel:matrikkel-kafka-light:${version}"
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kartverket/matrikkel-kafka-light")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
