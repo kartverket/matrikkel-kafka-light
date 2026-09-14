82abd9a184a7539f719240668b3f9bde933eda13
Mathias Gaptjern <Mathias.Gaptjern@kartverket.no>
2026-09-14 14:15:59 +0200

    Merge pull request #100 from kartverket/TH-3187
    TH-3187 Legger til oppsett for å publisere klienter til maven

--- libs/client/build.gradle.kts
+++ libs/client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("buildsrc.convention.kotlin-jvm")
+    id("maven-publish")
 }
 
 dependencies {
@@ -13,4 +14,42 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(libs.bundles.testEcosystem)
     testImplementation(libs.bundles.testEcosystem)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "no.kartverket.kafkalight"
+            artifactId = "client"
+            pom {
+                name.set("kafka-light-client")
+                description.set("Client for use with kafka light component")
+                url.set("https://github.com/kartverket/matrikkel-kafka-light")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/kartverket/heimdall-common-utils/blob/main/LICENSE")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/kartverket/matrikkel-kafka-light.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/kartverket/matrikkel-kafka-light.git")
+                    url.set("https://github.com/kartverket/matrikkel-kafka-light")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kartverket/matrikkel-kafka-light")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
