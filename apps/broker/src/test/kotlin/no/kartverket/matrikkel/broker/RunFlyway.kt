56472e5283f4b7809489499024f04a71d461590f
Nicklas Utgaard <nutgaard@gmail.com>
2026-09-23 10:14:37 +0200

    TH-3231: bump heimdall-common
    inkluderer sletting av litt utilities som nå finnes samlet i kotlin-utils

--- apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/RunFlyway.kt
+++ apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/RunFlyway.kt
@@ -1,15 +1,16 @@
 package no.kartverket.no.kartverket.matrikkel.broker
 
-import no.kartverket.heimdall.common.ktor.utils.EnvUtils.getRequiredConfig
+import no.kartverket.heimdall.common.kotlin.EnvUtils
+import no.kartverket.heimdall.common.kotlin.EnvUtils.getConfig
 import no.kartverket.matrikkel.broker.config.Credential
 import no.kartverket.matrikkel.broker.config.DataSourceConfiguration
 import no.kartverket.matrikkel.broker.config.DatabaseConfiguration
 
 fun main() {
-    Env.load("docker/local-postgres.env")
+    EnvUtils.load("docker/local-postgres.env")
 
     val database = DatabaseConfiguration(
-        jdbcUrl = getRequiredConfig("DB_URL"),
+        jdbcUrl = getConfig("DB_URL"),
         userCredential = Credential.from("DB_USER"),
         adminCredential = Credential.from("DB_ADMIN"),
     )
--- apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/RunLocally.kt
+++ apps/broker/src/test/kotlin/no/kartverket/matrikkel/broker/RunLocally.kt
@@ -1,9 +1,10 @@
 package no.kartverket.no.kartverket.matrikkel.broker
 
+import no.kartverket.heimdall.common.kotlin.EnvUtils
 import no.kartverket.matrikkel.broker.runApplication
 
 fun main() {
-     Env.load("docker/local-postgres.env")
+     EnvUtils.load("docker/local-postgres.env")
 //    Env.load("docker/local-h2.env")
     runApplication(disableSecurity = true)
 }
